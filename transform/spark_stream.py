3c1d9c09898744fdd0ad612155dbbfaf1ec4e972
Rais Muhammad <raisbaig13@gmail.com>
2024-08-27 00:36:17 +0500

    set up the spark master and worker architecture and test it by using spamle code

--- transform/spark_stream.py
+++ transform/spark_stream.py
@@ -1,8 +1,16 @@
+#!/usr/bin/env python
 import os
 import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'setup')))
+try:
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'setup')))
+    from sql_conn import sql_server_connect
+except ImportError as e:
+    print(f"Error importing module: {e}")
+    sys.exit(1)
 
-from sql_conn import sql_server_connect
-
+print("############################hello this is a spark code running ############################")
 sql_server_connect()
+
+
+
